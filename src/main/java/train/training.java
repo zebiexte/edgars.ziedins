07b17787fe82fb237559cb5a8755b4c117a9cdca
zebiexte <edgars.ziedins1@gmail.com>
2022-05-19 22:29:06 +0300

    Homework 4

---
+++ src/main/java/train/training.java
@@ -0,0 +1,12 @@
+package train;
+
+public class training {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
+        int x = b + 15;
+        System.out.println(x);
+    }
+}
